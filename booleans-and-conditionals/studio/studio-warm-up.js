6962a7226da2c8a79da86f02e4f969d899230e75
Matthew Creek <matthew.creek27@gmail.com>
2024-03-27 12:00:08 -0500

    beginning of the day sync

---
+++ booleans-and-conditionals/studio/studio-warm-up.js
@@ -0,0 +1,12 @@
+
+
+let weather = 'cloudy';
+let temp = 56;
+
+if (weather === 'clear' && temp > 65) {
+	console.log("Let's go for a walk!");
+} else console.log("I think I'll stay inside...");
+
+//what is the output of this code block?
+
+//what would we change to change the output?
