39733cbddcb86eba9df035241b99cfb40854082c
Matthew Creek <matthew.creek27@gmail.com>
2024-03-28 10:47:07 -0500

    multidimensional arrays studio

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -18,9 +18,17 @@ let cargoHold = [food, equipment, pets, sleepAids]
 let userInput = input.question('What drawer would you like to view: 0, 1, 2, or 3? ')
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-if (userInput >= 0 && userInput < 4) {
-    console.log(cargoHold[userInput])
-}
 
+// if (userInput >= 0 && userInput < 4) {
+//     console.log(`Your selected drawer contains ${cargoHold[userInput][0]}, ${cargoHold[userInput][1]}, ${cargoHold[userInput][2]}, and ${cargoHold[userInput][3]}.`)
+// }
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
+let userItem = input.question(`What item are you looking for? `)
+
+if (userInput >= 0 && userInput < 4) {
+    if (cargoHold[userInput].includes(userItem)) {
+        console.log(`Cabinet ${userInput} does contain ${userItem}.`)
+    } else console.log(`Cabinet ${userInput} does not contain ${userItem}.`)
+} else console.log("Drawer does not exist!")
