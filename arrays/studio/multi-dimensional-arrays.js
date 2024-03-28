054c955d9c35048f4340253f40bf74320befebbd
Matthew Creek <matthew.creek27@gmail.com>
2024-03-28 10:31:33 -0500

    gitignore added

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,26 @@
+const input = require('readline-sync')
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+food = food.split(',')
+equipment = equipment.split(',')
+pets = pets.split(',')
+sleepAids = sleepAids.split(',')
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [food, equipment, pets, sleepAids]
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+let userInput = input.question('What drawer would you like to view: 0, 1, 2, or 3? ')
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+if (userInput >= 0 && userInput < 4) {
+    console.log(cargoHold[userInput])
+}
+
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
