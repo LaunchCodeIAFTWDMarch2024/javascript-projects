07c092d7d6cb24c368e1309b3867ba892ee0cfb2
Matthew Creek <matthew.creek27@gmail.com>
2024-03-28 09:55:45 -0500

    studio part 1

---
+++ arrays/studio/array-studio-warmup.js
@@ -0,0 +1,12 @@
+let str = "LaunchCode";
+let newStr = str.split('').join('_');
+console.log(newStr);
+// What is the output?
+
+let refrigerator = [
+	['apple','banana','orange'],
+	['milk','v8','Calpico'],
+	['butter','ketchup','mustard']
+];
+console.log(refrigerator[1][2]);
+//what is the output?
