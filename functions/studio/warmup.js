578398899d8e15be89d01c7f6d1e185809801ad0
Matthew Creek <matthew.creek27@gmail.com>
2024-04-02 13:30:26 -0500

    for exercises complete

---
+++ functions/studio/warmup.js
@@ -0,0 +1,9 @@
+
+//attendance code: FantasticForLoop
+
+function calculateMPG(distanceTraveled, fuel) {
+	let milesPerGallon = distanceTraveled/fuel;
+	return milesPerGallon;
+}
+
+//what is the displayed output of this code snippet?
