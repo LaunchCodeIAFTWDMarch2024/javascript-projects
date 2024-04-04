8031ff735aa1a610ed92686e966090e4bb9ded50
Matthew Creek <matthew.creek27@gmail.com>
2024-04-04 11:08:58 -0500

    added studio warmup

---
+++ more-on-functions/studio/warmup.js
@@ -0,0 +1,40 @@
+
+
+//attendance code: FocusedFunctions
+
+let miscArr = ['hello world!', 16, true, 'Oranges']
+
+let filteredArr = miscArr.filter((element) => {
+    return typeof element !== 'number'
+});
+
+// console.log(filteredArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let getFiltered = function(element) {
+    return typeof element !== 'boolean';
+}
+
+// console.log(miscArr.filter(getFiltered));
+
+
+
+function filterElement(element) {
+    return typeof element !== 'string'
+}
+
+// console.log(miscArr.filter(filterElement))
