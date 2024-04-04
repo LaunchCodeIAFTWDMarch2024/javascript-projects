af7c7e3faaca8fda84683e7b95a86c1b34f97b31
Matthew Creek <matthew.creek27@gmail.com>
2024-04-04 12:06:12 -0500

    added warmup for object studio

---
+++ objects-and-math/studio/warmup.js
@@ -0,0 +1,16 @@
+
+
+//attendance code:
+
+let helperFunctions = {
+    addTwo: function(num1, num2){ return num1 + num2 },
+    filterNums: function(arr){return arr.filter((e) => typeof e !== 'number')},
+    message: 'Hope these functions help!',
+}
+
+console.log(helperFunctions.addTwo(3,6))
+//what's the expected output?
+console.log(helperFunctions.filterNums(['one', 2, 'three', 4, false, '']))
+//what's the expected output?
+console.log(helperFunctions.message)
+//what's the expected output?
