68711a0cc3ee2be421049d71875d2dea6a3e11de
Matthew Creek <matthew.creek27@gmail.com>
2024-04-04 09:55:20 -0500

    moron functions part 1

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -5,6 +5,20 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
+function findMinVal(arr) {
+    let minVal = Infinity
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i+1] && arr[i+1] < minVal) {
+            minVal = arr[i+1];
+        } else if (arr[i] < minVal) {
+            minVal = arr[i]
+        } 
+    }
+    return minVal;
+}
+
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(findMinVal(nums1));
+console.log(findMinVal(nums2));
+console.log(findMinVal(nums3));
