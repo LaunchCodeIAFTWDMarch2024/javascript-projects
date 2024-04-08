6e067339c8367a2dfe97ee90f7f40cd38154513b
Matthew Creek <matthew.creek27@gmail.com>
2024-04-08 14:44:26 -0500

    part three complete

--- objects-and-math/studio/ObjectsStudio03.js
+++ objects-and-math/studio/ObjectsStudio03.js
@@ -1,8 +1,25 @@
 // Code your crewMass function here:
-
+function crewMass(arr) {
+  let totalCrewMass = 0;
+  for (let i = 0; i < arr.length; i++) {
+    totalCrewMass += arr[i].mass
+  }
+  return Math.round(totalCrewMass * 10) / 10;
+}
 
 // Code your fuelRequired function here:
-
+function fuelRequired(arr) {
+  let shuttleMass = 75000
+  let totalMass = shuttleMass + crewMass(arr)
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].species === 'dog' || arr[i].species === 'cat') {
+      totalMass += 200
+    } else {
+      totalMass += 100
+    }
+  }
+  return Math.ceil(totalMass * 9.5)
+}
 
 // The pre-selected crew is in the array at the end of this file.
 // Feel free to add, remove, or switch crew members as you see fit.
@@ -52,3 +69,7 @@ let candidateA = {
  
  let crew = [candidateB,candidateD,candidateF];
  
+//  console.log(crewMass(crew))
+//  console.log(fuelRequired(crew))
+
+ console.log(`The mission has a launch mass of ${crewMass(crew) + 75000} kg and requires ${fuelRequired(crew)} kg of fuel.`)
