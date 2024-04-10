415f3fbfabf58f7b6fcc28ee34ef129fac585196
Matthew Creek <matthew.creek27@gmail.com>
2024-04-10 16:05:54 -0500

    classes studio warmup

---
+++ classes/studio/warmup.js
@@ -0,0 +1,19 @@
+//attendance code:
+
+class Vehicle {
+    constructor(hasWheels, isPowered, capacity){
+        this.hasWheels = hasWheels,
+        this.isPowered = isPowered,
+        this.capacity = capacity
+    }
+}
+
+class Car extends Vehicle {
+    constructor(numWheels) {
+        super(true, true, 2),
+        this.numWheels = numWheels
+    }
+}
+
+let ferrari = new Car(4)
+console.log(ferrari)
