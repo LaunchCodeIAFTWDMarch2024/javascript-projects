d4607cc7ce6692ee9c7efbe393a9d941fd653934
Matthew Creek <matthew.creek27@gmail.com>
2024-04-10 10:02:54 -0500

    unit testing

---
+++ examples/user-input.js
@@ -0,0 +1,20 @@
+
+
+// we have a question that is going to have an answer that is either correct or incorrect
+
+// if (correctAnswer === userAnswer) {
+//     //if true, this code runs and stops
+// } else {
+//     //if first conditional is not true, this code runs
+// }
+
+// if (userAnswer >= 5) {
+//     //if user enters a number 5 or higher this code runs
+// } else if (userAnswer > 3) {
+//     //if user enters a number above 3
+// } else {
+    
+// }
+
+let a = 14
+console.log(a > 20 === true);
