8358bdf51a85174878a3f1fbecd53e178544d28d
Matthew Creek <matthew.creek27@gmail.com>
2024-04-10 15:58:44 -0500

    unit testing warmup

---
+++ unit-testing/studio/tests/warmup.test.js
@@ -0,0 +1,11 @@
+
+
+//attendance code: 
+
+const warmup = require('../warmup.js')
+
+describe('testing warmup', function() {
+    test('function return should be a string', function() {
+        expect(typeof warmup).toBe('string')
+    })
+})
