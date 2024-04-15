365fe9d44a3143443b94ef90169909477ba9d545
Matthew Creek <matthew.creek27@gmail.com>
2024-04-15 15:41:54 -0500

    misc work across chapters

--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -1,5 +1,21 @@
 function reverse(str) {
+   //str = 'abc'
    let lettersArray = str.split('');
+   //letterArray = ['a','b','c']
    let reversedLettersArray = lettersArray.reverse();
+   //reversedLettersArray = ['c','b','a']
    return reversedLettersArray.join('');
+   // return 'cba'
 }
+
+let str = 'abca;lfghf;lgajf;glajdf;lgafkadfgoiajdfl;gkadflgad;igajd;ligj'
+
+// console.log(reverse('abc'))
+
+let reverseStr = reverse(str)
+
+console.log(reverseStr)
+
+str = 'apple'
+
+console.log(reverse(str))
