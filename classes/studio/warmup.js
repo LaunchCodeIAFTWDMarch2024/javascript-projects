b42aaaf4ed036fb906491a93498d78832a3ecb97
Matthew Creek <matthew.creek27@gmail.com>
2024-04-16 16:56:18 -0500

    studio code along complete

--- classes/studio/warmup.js
+++ classes/studio/warmup.js
@@ -1,19 +1,20 @@
+
+
 //attendance code: CodingClasses
 
 class Vehicle {
     constructor(hasWheels, isPowered, capacity){
-        this.hasWheels = hasWheels,
-        this.isPowered = isPowered,
-        this.capacity = capacity
+        this.hasWheels = hasWheels;
+        this.isPowered = isPowered;
+        this.capacity = capacity;
     }
 }
-
 class Car extends Vehicle {
     constructor(numWheels) {
-        super(true, true, 2),
-        this.numWheels = numWheels
+        super(true, true, 2);
+        this.numWheels = numWheels;
     }
 }
-
 let ferrari = new Car(4)
+let example = new Vehicle()
 console.log(ferrari)
