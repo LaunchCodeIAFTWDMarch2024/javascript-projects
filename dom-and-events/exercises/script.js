8e29d4dc2ae03b5c214791f3506357c59984ec3d
Matthew Creek <matthew.creek27@gmail.com>
2024-04-23 10:06:35 -0500

    dom exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+    paragraph.innerHTML = 'Houston! We have liftoff!';
+    })
+
+    missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.background = 'red'
+    })
+    missionAbort.addEventListener('mouseout', event => {
+        missionAbort.style.background = ''
+    })
+
+    missionAbort.addEventListener('click', event => {
+        if (confirm("Are you sure you want to abort mission?")){
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home'
+        }
+    })
+
 }
 
 window.addEventListener("load", init);
+
+
